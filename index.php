548252e393fa35dd07406f2098faf7ac1297441e
Jinghao1209 <84082781+Jinghao1209@users.noreply.github.com>
2021-10-15 19:09:13 +0800

    add all and update all...

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!-- Dont delete your localStorage id! -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- <meta http-equiv="refresh" content="3"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POP</title>
+    <link rel="stylesheet" href="./client.css">
+</head>
+<body onload="loading()">
+<p>POP</p>
+<h3 id="ClickTime"></h3>
+<div data-people="1"></div>
+</body>
+</html>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+<script src="./id.js"></script>
+<script src="./listen.js"></script>
+<script src="./watch.js"></script>
