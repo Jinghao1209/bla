9bfb9b5544c7d7919ad1a38446177cccf2ad91bd
Jinghao1209 <84082781+Jinghao1209@users.noreply.github.com>
2021-10-15 19:08:00 +0800

    update

--- public/action.php
+++ public/action.php
@@ -1,19 +1,29 @@
 <?php
 
 session_start();
+header('Content-Type: application/json');
 $brk = true;
-function a($data){
+$data = json_decode(file_get_contents("php://input"), true);
+$sqlselect = "SELECT * FROM db;";
+$conn = mysqli_connect("localhost", "root", "", "test_db");
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
+
+function a($data) {
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
 }
-if (isset($_POST['id']) && isset($_POST['click'])) {
-    $_SESSION['id'] = a($_POST['id']);
-    $_POST['click'] = a($_POST['click']);
+if (isset($data['id']) && isset($data['click'])) {
+    $_SESSION['id'] = a($data['id']);
+    $data['click'] = a($data['click']);
     if (isset($_SESSION['click'])) {
-        if (intval($_POST['click']) - intval($_SESSION['click']) == 1) {
-            $_SESSION['click'] = $_POST['click'];
+        if (intval($data['click']) - intval($_SESSION['click']) == 1) {
+            $_SESSION['click'] = $data['click'];
         }
     } else {
         $_SESSION['click'] = 1;
@@ -23,25 +33,32 @@ if (isset($_POST['id']) && isset($_POST['click'])) {
     $json = json_decode($info);
     $_SESSION['country'] = $json->country;
 
-    $conn = mysqli_connect("localhost", "root", "", "test_db");
-    // Check connection
-    if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-        exit();
-    }
-    
-    $sqlselect = "SELECT * FROM db;";
     $result = mysqli_query($conn, $sqlselect);
     while ($row = mysqli_fetch_assoc($result)) {
         if ($row['id'] === $_SESSION['id']) {
-            $sql = "UPDATE db SET click='".$_SESSION['click']."' WHERE id='".$_SESSION['id']."';";
+            $sql = "UPDATE db SET click='" . $_SESSION['click'] . "' WHERE id='" . $_SESSION['id'] . "';";
             mysqli_query($conn, $sql);
             $brk = false;
             break;
         }
     }
     if ($brk) {
-        $sql = "INSERT INTO db (id, click, country) VALUES ('".$_SESSION['id']."', '".$_SESSION['click']."','".$_SESSION['country']."');";
+        $sql = "INSERT INTO db (id, click, country) VALUES ('" . $_SESSION['id'] . "', '" . $_SESSION['click'] . "','" . $_SESSION['country'] . "');";
         mysqli_query($conn, $sql);
     }
+}
+$find = false;
+$result = mysqli_query($conn, $sqlselect);
+while ($row = mysqli_fetch_assoc($result)) {
+    if (isset($_SESSION['id'])) {
+        if ($row['id'] === $_SESSION['id']) {
+            $click = $row['click'];
+            echo json_encode(['click' => intval($click)]);
+            $find = true;
+            break;
+        }
+    }
+}
+if (!$find) {
+    echo json_encode(['click' => 0]);
 }
--- src/App.js
+++ src/App.js
@@ -2,64 +2,133 @@ import mdl from './modules';
 import axios from 'axios';
 import randomuuid from './randomuuid';
 import Leaderboard from './leaderboard';
+//#region CSS
+import Character_1 from './people/character/Character 500x500-1.png';
+import Character_2 from './people/character/Character 500x500-2.png';
+import Character_3 from './people/character/Character 500x500-3.png';
+import Character_4 from './people/character/Character 500x500-4.png';
+import Character_5 from './people/character/Character 500x500-5.png';
+import Character_6 from './people/character/Character 500x500-6.png';
+import Character_7 from './people/character/Character 500x500-7.png';
+import Character_8 from './people/character/Character 500x500-8.png';
+import Character_9 from './people/character/Character 500x500-9.png';
+import Character_10 from './people/character/Character 500x500-10.png';
+import Character_11 from './people/character/Character 500x500-11.png';
+import Character_12 from './people/character/Character 500x500-12.png';
+import Character_13 from './people/character/Character 500x500-13.png';
+import Character_14 from './people/character/Character 500x500-14.png';
+import Character_15 from './people/character/Character 500x500-15.png';
+import Character_16 from './people/character/Character 500x500-16.png';
+import Character_17 from './people/character/Character 500x500-17.png';
+import Character_18 from './people/character/Character 500x500-18.png';
+import Character_19 from './people/character/Character 500x500-19.png';
+import Character_20 from './people/character/Character 500x500-20.png';
+import Character_21 from './people/character/Character 500x500-21.png';
+import Character_22 from './people/character/Character 500x500-22.png';
+import Character_23 from './people/character/Character 500x500-23.png';
+import Character_24 from './people/character/Character 500x500-24.png';
+import Character_25 from './people/character/Character 500x500-25.png';
+import Character_26 from './people/character/Character 500x500-26.png';
+import Character_27 from './people/character/Character 500x500-27.png';
+import Character_28 from './people/character/Character 500x500-28.png';
+import Character_29 from './people/character/Character 500x500-29.png';
+import Character_30 from './people/character/Character 500x500-30.png';
 import './index.css';
+//#endregion
 
-var downX, downY, upX, upY, placeX, placeY, lastXY, clicked = false;
+let downX, downY, upX, upY, placeX, placeY, lastXY, clicked = false;
 function App() {
     if (localStorage.getItem('id') == undefined) randomuuid();
-    const [clicktime, changeClickTime] = mdl.React.useState(Number(localStorage.getItem('click')))
+    const [clicktime, changeClickTime] = mdl.React.useState(parseInt(localStorage.getItem('click')))
     const [checkeff, changeCheckEff] = mdl.React.useState(0)
     const [windowHeight, changeWindowHeight] = mdl.React.useState(window.innerHeight)
     const [windowWidth, changeWindowWidth] = mdl.React.useState(window.innerWidth)
     const [baropen, changeBarOpenStatus] = mdl.React.useState(false)
     const [barClass, changeBarClass] = mdl.React.useState("bar")
     const [Worldwide, changeWorldwide] = mdl.React.useState(0)
+    const [characterSrc, setCharacterSrc] = mdl.React.useState(Character_1);
 
     //#region listen
     mdl.React.useEffect(async () => {
         if (clicked) {
-            localStorage.setItem('click', clicktime)
-            var Formdata = new FormData();
-            Formdata.append("id", localStorage.getItem('id'));
-            Formdata.append("click", clicktime);
-            var req = new XMLHttpRequest();
-            req.open("POST", "/action.php");
-            req.send(Formdata);
-
-            var res = await axios.get('/clicktime.php');
-            var data = res.data.click
-            localStorage.setItem('click', data)
-            clicked = false;
+            localStorage.setItem('click', clicktime);
+            axios.post("/action.php", {
+                id: localStorage.getItem('id'),
+                click: clicktime
+            }).then(res => {
+                let data = res.data.click;
+                localStorage.setItem('click', data);
+                clicked = false;
+            })
         }
     }, [checkeff])
+    // Render worldwide
     mdl.React.useEffect(() => {
         axios.get('/select.php').then(res => changeWorldwide(res.data))
     })
-    mdl.React.useEffect(async () => {
-        axios.get('/clicktime.php').then(res => {
-            localStorage.setItem('click', Number(res.data.click))
-            changeClickTime(Number(res.data.click))
+    // Render click time
+    mdl.React.useEffect(() => {
+        axios.get('/action.php').then(res => {
+            localStorage.setItem('click', parseInt(res.data.click))
+            changeClickTime(parseInt(res.data.click))
         })
+        changeCharacter(false);
     }, [])
-    var resize = (e) => {
+    // character
+    mdl.React.useEffect(() => changeCharacter, [clicktime])
+    const resize = (e) => {
         changeWindowWidth(window.innerWidth)
         changeWindowHeight(window.innerHeight)
     }
     mdl.React.useEffect(() => {
         window.addEventListener("resize", resize)
-        return () => {
-            window.removeEventListener("resize", resize)
-        }
+        return () => window.removeEventListener("resize", resize)
     })
     //#endregion
 
     //#region function
     /**
+     * @param {boolean} beaten 
+     */
+    const changeCharacter = (beaten) => {
+        if (clicktime <= 100) { // 1...100
+            !beaten ? setCharacterSrc(Character_1) : setCharacterSrc(Character_2);
+        } else if (clicktime > 100 && clicktime <= 200) { // 101...200
+            !beaten ? setCharacterSrc(Character_3) : setCharacterSrc(Character_4);
+        } else if (clicktime > 200 && clicktime <= 300) { // 201...300...
+            !beaten ? setCharacterSrc(Character_5) : setCharacterSrc(Character_6);
+        } else if (clicktime > 300 && clicktime <= 400) {
+            !beaten ? setCharacterSrc(Character_7) : setCharacterSrc(Character_8);
+        } else if (clicktime > 400 && clicktime <= 500) {
+            !beaten ? setCharacterSrc(Character_9) : setCharacterSrc(Character_10);
+        } else if (clicktime > 500 && clicktime <= 600) {
+            !beaten ? setCharacterSrc(Character_11) : setCharacterSrc(Character_12);
+        } else if (clicktime > 600 && clicktime <= 700) {
+            !beaten ? setCharacterSrc(Character_13) : setCharacterSrc(Character_14);
+        } else if (clicktime > 700 && clicktime <= 800) {
+            !beaten ? setCharacterSrc(Character_15) : setCharacterSrc(Character_16);
+        } else if (clicktime > 800 && clicktime <= 900) {
+            !beaten ? setCharacterSrc(Character_17) : setCharacterSrc(Character_18);
+        } else if (clicktime > 900 && clicktime <= 1000) {
+            !beaten ? setCharacterSrc(Character_19) : setCharacterSrc(Character_20);
+        } else if (clicktime > 1000 && clicktime <= 2000) {
+            !beaten ? setCharacterSrc(Character_21) : setCharacterSrc(Character_22);
+        } else if (clicktime > 2000 && clicktime <= 3000) {
+            !beaten ? setCharacterSrc(Character_23) : setCharacterSrc(Character_24);
+        } else if (clicktime > 3000 && clicktime <= 4000) {
+            !beaten ? setCharacterSrc(Character_25) : setCharacterSrc(Character_26);
+        } else if (clicktime > 4000 && clicktime <= 5000) {
+            !beaten ? setCharacterSrc(Character_27) : setCharacterSrc(Character_28);
+        } else if (clicktime > 5000 && clicktime <= 6000) {
+            !beaten ? setCharacterSrc(Character_29) : setCharacterSrc(Character_30);
+        }
+    }
+    /**
      * mousedown
      * @param {MouseEvent} e 
      */
     const mousedown = (e) => {
-        document.getElementsByClassName('image')[0].style.backgroundImage = 'url(https://popcat.click/img/op.353767c3.png)';
+        changeCharacter(true);
         downX = parseInt(e.clientX);
         downY = parseInt(e.clientY);
     }
@@ -67,8 +136,7 @@ function App() {
      * mouseup
      * @param {MouseEvent} e 
      */
-    const mouseup = async (e) => {
-        document.getElementsByClassName('image')[0].style.backgroundImage = 'url(https://popcat.click/img/p.1e9d00be.png)';
+    const mouseup = (e) => {
         upX = parseInt(e.clientX)
         upY = parseInt(e.clientY)
         upX > downX ? placeX = upX - downX : placeX = downX - upX;
@@ -85,7 +153,8 @@ function App() {
             changeClickTime(clicktime + 1)
             changeCheckEff(checkeff + 1)
         }
-    }
+        changeCharacter(false);
+    };
     const barclick = () => {
         if (!baropen) {
             changeBarOpenStatus(true)
@@ -105,8 +174,9 @@ function App() {
             onMouseUp={(e) => mouseup(e)}
         >
             <p>POP</p>
-            <div className={'img image'}>
-                <div id="ClickTime" className={'ClickTime'}>{clicktime}</div>
+            <div className="img">
+                <div id="ClickTime" className="ClickTime">{clicktime}</div>
+                <img src={characterSrc} />
             </div>
             <section data-leaderboard style={{ display: 'block' }}>
                 {/* ≔ */}
@@ -116,7 +186,7 @@ function App() {
                         🌍 Worldwide:&nbsp;{Worldwide}
                     </div>
                     <div data-pull>
-                        <button className={"show"}>
+                        <button className="show">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"></path>
                             </svg>
